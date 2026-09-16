a55c465c4dc59e48f4ad9d4e7d94cecf514c9866
praful-devv <prafulanilwankhade3010@gmail.com>
2026-09-16 15:42:24 +0530

    first commit

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const userModel = require("../models/users.model")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const Router = express.Router()
+
+Router.post("/register",async(req,res)=>{
+    const {name,email,password} = req.body
+
+    const isUserExists = await userModel.findOne({email})
+
+    if(isUserExists){
+        return res.status(409).json({
+            message:"user already exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password,12)
+
+    const user = await userModel.create({
+        name,email,password:hash
+    })
+
+    const token = await jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRETS,
+      {expiresIn:"1h"}
+    )
+
+    res.cookie("jwt_token",token)
+
+    res.status(201).json({
+        message:"your account created successfully",
+        user
+    })
+})
+
+module.exports = Router
