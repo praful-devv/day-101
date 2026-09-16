7546a0d86a0b0719d87bfaf5a846ab894fa189df
praful-devv <prafulanilwankhade3010@gmail.com>
2026-09-16 18:59:52 +0530

    password validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,45 +1,61 @@
-const express = require("express")
-const userModel = require("../models/users.model")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const userModel = require("../models/users.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const Router = express.Router()
+const Router = express.Router();
 
-Router.post("/register",async(req,res)=>{
-    const { username, email, password, bio, profile_img} = req.body;
+Router.post("/register", async (req, res) => {
+  const { username, email, password, bio, profile_img } = req.body;
 
-    const isUserExists = await userModel.findOne({
-        $or:[
-            {username},{email}
-        ]
+  const isUserExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserExists) {
+    return res.status(409).json({
+      message:
+        isUserExists.email == email
+          ? "user already exists"
+          : "username is taken taken already",
+    });
+  } 
+
+  if(!password){
+    return res.status(400).json({
+        message:"password required"
     })
-   
-    if(isUserExists){
-        return res.status(409).json({
-            message:"user exists already"
-        })
+  }
+
+  const hash = await bcrypt.hash(password, 12);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+    bio,
+    profile_img,
+  });
+
+  const token = await jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRETS,
+    { expiresIn: "1h" },
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "your account created successfully",
+    user: {
+        username:user.username,
+        email:user.email,
+        profile_img:user.profile_img,
+        bio:user.bio
     }
+  });
+});
 
-    const hash = await bcrypt.hash(password,12)
-
-    const user = await userModel.create({
-        username,email,password:hash,bio,profile_img
-    })
-
-    const token = await jwt.sign(
-      {
-        id: user._id,
-      },
-      process.env.JWT_SECRETS,
-      {expiresIn:"1h"}
-    )
-
-    res.cookie("jwt_token",token)
-
-    res.status(201).json({
-        message:"your account created successfully",
-        user
-    })
-})
-
-module.exports = Router
+module.exports = Router;
